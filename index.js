fda8dc30d1f5c2003439adb4327173b5ba5b0f6d
Serkan Şahin <serkan.sahin@useinsider.com>
2024-04-07 18:17:09 +0300

    update response

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const axios = require('axios');
 
-exports.handler = async () => {
+exports.handler = async (event, context, callback) => {
     try {
         const answer = 'yes';
         const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jd6p6Jc6P1cvMdMSTqUfm1reDHHeLoff&q=${answer}&limit=1`);
 
-        return {
-            status: 200,
-            body: JSON.stringify({ answer, url: res.data.data[0].url })
-        }
+        return callback(null, { statusCode: 200, body: JSON.stringify({ answer, url: res.data.data[0].url }) });
     } catch (error) {
-        return {
-            status: 500,
-            body: JSON.stringify({ message: 'Something went wrong' })
-        }
+        return callback(null, { statusCode: 500, body: 'Something went wrong' });
     }
 }
