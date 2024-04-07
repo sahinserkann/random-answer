9d44f3fac4e403d52286cc844d64ff849fdf616f
Serkan Şahin <serkan.sahin@useinsider.com>
2024-04-07 18:44:09 +0300

    make it randomise

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const axios = require('axios');
 
 exports.handler = async (event, context, callback) => {
     try {
-        const answer = 'yes';
-        const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jd6p6Jc6P1cvMdMSTqUfm1reDHHeLoff&q=${answer}&limit=1`);
+        const answers = ['yes', 'no'];
+        const answer = answers[Math.floor(Math.random()*answers.length)];
+        const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jd6p6Jc6P1cvMdMSTqUfm1reDHHeLoff&q=${answer}`);
+        const items = res.data.data;
+        const item = items[Math.floor(Math.random()*items.length)];
 
-        return callback(null, { statusCode: 200, body: JSON.stringify({ answer, url: res.data.data[0].url }) });
+        return callback(null, { statusCode: 200, body: JSON.stringify({ answer, url: item.url }) });
     } catch (error) {
         return callback(null, { statusCode: 500, body: 'Something went wrong' });
     }
