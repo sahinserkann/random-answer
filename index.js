095abbe1bd1df3db310ef0f2debe3868465bfa6d
Serkan Şahin <serkan.sahin@useinsider.com>
2024-04-07 18:59:54 +0300

    add query params

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ exports.handler = async (event, context, callback) => {
     try {
         const apiKey = 'SnFkIteqzCUpPW5eHU6mFodmMPAkxUPu';
         const answers = ['yes', 'no'];
-        const answer = answers[Math.floor(Math.random()*answers.length)];
+        const predefinedAnswer = event?.queryStringParameters?.answer;
+        const answer = predefinedAnswer || answers[Math.floor(Math.random()*answers.length)];
         const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${answer}`);
         const items = res.data.data;
         const item = items[Math.floor(Math.random()*items.length)];
