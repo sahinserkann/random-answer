ba38debbd16a8fabd6c625d5348b2aefe976f4ba
Serkan Şahin <serkan.sahin@useinsider.com>
2024-04-07 18:08:11 +0300

    add try catch

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import axios from 'axios';
+const axios = require('axios');
 
-export const handler = async () => {
-    const answer = 'yes';
-    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jd6p6Jc6P1cvMdMSTqUfm1reDHHeLoff&q=${answer}&limit=1`);
+exports.handler = async () => {
+    try {
+        const answer = 'yes';
+        const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jd6p6Jc6P1cvMdMSTqUfm1reDHHeLoff&q=${answer}&limit=1`);
 
-    return {
-        status: 200,
-        body: JSON.stringify({ answer, url: res.data[0].url })
+        return {
+            status: 200,
+            body: JSON.stringify({ answer, url: res.data.data[0].url })
+        }
+    } catch (error) {
+        return {
+            status: 500,
+            body: JSON.stringify({ message: 'Something went wrong' })
+        }
     }
 }
