2cfcb1d9d9d508a733fbe6d63cf88c7986429612
Serkan Şahin <serkan.sahin@useinsider.com>
2024-04-07 18:56:16 +0300

    add 429 response check

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const axios = require('axios');
 
 exports.handler = async (event, context, callback) => {
     try {
+        const apiKey = 'SnFkIteqzCUpPW5eHU6mFodmMPAkxUPu';
         const answers = ['yes', 'no'];
         const answer = answers[Math.floor(Math.random()*answers.length)];
-        const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=jd6p6Jc6P1cvMdMSTqUfm1reDHHeLoff&q=${answer}`);
+        const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${answer}`);
         const items = res.data.data;
         const item = items[Math.floor(Math.random()*items.length)];
 
         return callback(null, { statusCode: 200, body: JSON.stringify({ answer, url: item.url }) });
     } catch (error) {
-        return callback(null, { statusCode: 500, body: 'Something went wrong' });
+        if (error.response.status === 429) {
+            return callback(null, { statusCode: 429, body: JSON.stringify(error.response.data.meta.msg) });
+        }
+
+        return callback(null, { statusCode: 500, body: 'Something went wrong!' });
     }
 }
